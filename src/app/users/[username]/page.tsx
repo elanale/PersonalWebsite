333a71a57358586384d86d1812f9fe03f68d4355
Elan <166648947+elanale@users.noreply.github.com>
2025-02-25 23:41:43 -0500

    Replace all files with Next.js project

--- src/app/users/[username]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { headers } from "next/headers";
-
-export default function Users() {
-  const headerList = headers();
-
-  const hostname = headerList.get("host");
-
-  return <h1>Hello from subdomain {hostname}</h1>;
-}
